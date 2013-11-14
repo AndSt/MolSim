372d1db2fffff55877582f5fbb71cf214d4b4d34
AndSt <andreas.stephan@tum.de>
2013-11-14 02:38:01 +0100

    add constructor

--- src/Cuboid.cpp
+++ src/Cuboid.cpp
@@ -11,7 +11,13 @@
 #include "utils/Vector.h"
 #include "MaxwellBoltzmannDistribution.h"
 
-Cuboid::Cuboid(int height, int width, int depth, double distance, double mass, utils::Vector<double, 3> ori, utils::Vector<double, 3> startVelocity, double meanVelocity){
+Cuboid::Cuboid(){
+
+}
+
+Cuboid::Cuboid(int height, int width, int depth, double distance, double mass,
+		utils::Vector<double, 3> ori, utils::Vector<double, 3> startVelocity,
+		double meanVelocity) {
 	// Initialize first variables
 	cHeight = height;
 	cWidth = width;
@@ -26,12 +32,12 @@ Cuboid::Cuboid(int height, int width, int depth, double distance, double mass, u
 	cub.clear();
 
 	// Initialize particles in cub
-	for (int hei = 0; hei < height; hei++){
-		for (int w = 0; w < width; w++){
-			for (int d = 0; d < depth; d++){
+	for (int hei = 0; hei < height; hei++) {
+		for (int w = 0; w < width; w++) {
+			for (int d = 0; d < depth; d++) {
 				// Must set each particle with its own coordinate
 				// Ox along width, Oy along height, Oz along depth
-				double addTemp[] = {w*h, hei*h, d*h};
+				double addTemp[] = { w * h, hei * h, d * h };
 				utils::Vector<double, 3> addVector(addTemp);
 				utils::Vector<double, 3> vel(ori + addVector);
 
@@ -47,110 +53,110 @@ Cuboid::Cuboid(int height, int width, int depth, double distance, double mass, u
 }
 
 /*
-Cuboid::Cuboid(int height, int width, int depth, double distance, double mass, utils::Vector<double, 3> ori, utils::Vector<double, 3> startVelocity, double meanVelocity){
-	// Initialize first variables
-	cHeight = height;
-	cWidth = width;
-	cDepth = depth;
-	origin = ori;
-	h = distance;
-	m = mass;
-	startV = startVelocity;
-	meanV = meanVelocity;
+ Cuboid::Cuboid(int height, int width, int depth, double distance, double mass, utils::Vector<double, 3> ori, utils::Vector<double, 3> startVelocity, double meanVelocity){
+ // Initialize first variables
+ cHeight = height;
+ cWidth = width;
+ cDepth = depth;
+ origin = ori;
+ h = distance;
+ m = mass;
+ startV = startVelocity;
+ meanV = meanVelocity;
 
-	// Initialize cub
-	cub.resize(height);
-	for (int i = 0; i < height; i++) {
-		cub[i].resize(width);
+ // Initialize cub
+ cub.resize(height);
+ for (int i = 0; i < height; i++) {
+ cub[i].resize(width);
 
-		for (int j = 0; j < width; j++)
-		   	cub[i][j].resize(depth);
-	}
+ for (int j = 0; j < width; j++)
+ cub[i][j].resize(depth);
+ }
 
-	// Initialize particles in cub
-	for (int hei = 0; hei < height; hei++){
-		for (int w = 0; w < width; w++){
-			for (int d = 0; d < depth; d++){
-				cub[hei][w][d] = Particle();
-				cub[hei][w][d].setM(mass);
-				cub[hei][w][d].getV() = startVelocity;
+ // Initialize particles in cub
+ for (int hei = 0; hei < height; hei++){
+ for (int w = 0; w < width; w++){
+ for (int d = 0; d < depth; d++){
+ cub[hei][w][d] = Particle();
+ cub[hei][w][d].setM(mass);
+ cub[hei][w][d].getV() = startVelocity;
 
-				// Must set each particle with its own coordinate
-				// Ox along width, Oy along height, Oz along depth
-				double addTemp[] = {w*h, hei*h, d*h};
-				utils::Vector<double, 3> addVector(addTemp);
-				cub[hei][w][d].getX() = ori + addVector;
+ // Must set each particle with its own coordinate
+ // Ox along width, Oy along height, Oz along depth
+ double addTemp[] = {w*h, hei*h, d*h};
+ utils::Vector<double, 3> addVector(addTemp);
+ cub[hei][w][d].getX() = ori + addVector;
 
-				// Movement of each particle superposed by Brownian Motion
-				MaxwellBoltzmannDistribution(cub[hei][w][d], meanVelocity, 3);
-			}
-		}
-	}
-}
-*/
+ // Movement of each particle superposed by Brownian Motion
+ MaxwellBoltzmannDistribution(cub[hei][w][d], meanVelocity, 3);
+ }
+ }
+ }
+ }
+ */
 
-utils::Vector<double, 3>& Cuboid::getOrigin(){
+utils::Vector<double, 3>& Cuboid::getOrigin() {
 	return origin;
 }
 
-utils::Vector<double, 3>& Cuboid::getStartV(){
+utils::Vector<double, 3>& Cuboid::getStartV() {
 	return startV;
 }
 
-std::list<Particle>& Cuboid::getCuboid(){
+std::list<Particle>& Cuboid::getCuboid() {
 	return cub;
 }
 
-int Cuboid::getHeight(){
+int Cuboid::getHeight() {
 	return cHeight;
 }
 
-void Cuboid::setHeight(double newH){
+void Cuboid::setHeight(double newH) {
 	cHeight = newH;
 }
 
-int Cuboid::getWidth(){
+int Cuboid::getWidth() {
 	return cWidth;
 }
 
-void Cuboid::setWidth(double newW){
+void Cuboid::setWidth(double newW) {
 	cWidth = newW;
 }
 
-int Cuboid::getDepth(){
+int Cuboid::getDepth() {
 	return cDepth;
 }
 
-void Cuboid::setDepth(double newD){
+void Cuboid::setDepth(double newD) {
 	cDepth = newD;
 }
 
-double Cuboid::getDistance(){
+double Cuboid::getDistance() {
 	return h;
 }
 
-void Cuboid::setDistance(double newD){
+void Cuboid::setDistance(double newD) {
 	h = newD;
 }
 
-double Cuboid::getMass(){
+double Cuboid::getMass() {
 	return m;
 }
 
-void Cuboid::setMass(double newM){
+void Cuboid::setMass(double newM) {
 	m = newM;
 }
 
-double Cuboid::getMeanV(){
+double Cuboid::getMeanV() {
 	return meanV;
 }
 
-void Cuboid::setMeanV(double newV){
+void Cuboid::setMeanV(double newV) {
 	meanV = newV;
 }
 
-int Cuboid::getSize(){
-	return cHeight*cWidth*cDepth;
+int Cuboid::getSize() {
+	return cHeight * cWidth * cDepth;
 }
 
 Cuboid::~Cuboid() {
