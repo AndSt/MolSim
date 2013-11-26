f238a6b3dffb29956e9f3ed7d4626e7c09a88ea8
n9s2cya <n9s2cya@hotmail.com>
2013-11-26 01:45:33 +0100

    Input folder added (contains input files for setting, particles and cuboids.

---
+++ input/cxx/tree/cuboids/driver.cxx
@@ -0,0 +1,26 @@
+// file      : examples/cxx/tree/hello/driver.cxx
+// author    : Boris Kolpackov <boris@codesynthesis.com>
+// copyright : not copyrighted - public domain
+
+
+#include <memory>   // std::auto_ptr
+#include <iostream>
+#include <string>
+#include <list>
+
+#include "InputCuboids.hxx"
+//#include "Vector.h"
+//#include "Cuboid.h"
+
+using namespace std;
+
+//void extractCuboids(list<Cuboid>& cub);
+
+
+int main(){
+	//list<Cuboid> cub;
+	//extractCuboids(cub);
+	return 0;
+}
+
+
